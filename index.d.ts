b6fe9171239d95ffb4b22d68fc4de0ddfc4fc592
Shinpei <32737669+zusinShinpei@users.noreply.github.com>
2019-06-15 00:53:49 +0900

    create index.d.ts
    it can be use from typescript

---
+++ index.d.ts
@@ -0,0 +1,8 @@
+import { NativeModulesStatic } from 'react-native';
+
+export const RNCookieManagerIOS: NativeModulesStatic;
+export const RNCookieManagerAndroid: NativeModulesStatic;
+
+export const CookieManager: NativeModulesStatic;
+
+export const functions: string[];
