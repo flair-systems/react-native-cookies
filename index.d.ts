54db19e5d9b654b93e856f084f73e2301c45a47a
Shinpei <32737669+zusinShinpei@users.noreply.github.com>
2019-06-15 01:21:11 +0900

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,15 @@
 import { NativeModulesStatic } from 'react-native';
 
-export const RNCookieManagerIOS: NativeModulesStatic;
-export const RNCookieManagerAndroid: NativeModulesStatic;
+export const RNCookieManagerIOS: NativeModulesStatic.RNCookieManagerIOS;
+export type RNCookieManagerIOS = NativeModulesStatic.RNCookieManagerIOS;
 
-export const CookieManager: NativeModulesStatic;
+export const RNCookieManagerAndroid: NativeModulesStatic.RNCookieManagerAndroid;
+export type RNCookieManagerAndroid = NativeModulesStatic.RNCookieManagerAndroid;
 
 export const functions: string[];
+export type functions = string[];
+
+export const CookieManager: NativeModulesStatic;
+export interface CookieManager {
+  [key: functions]: any;
+}
