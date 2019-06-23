bcd7a4962c672b14e567b4699fea760cee7554e9
Edward Paget <ed.paget@gmail.com>
2019-06-23 14:07:23 -0500

    Change how exports work

--- index.js
+++ index.js
@@ -7,30 +7,32 @@ const RNCookieManagerAndroid = NativeModules.RNCookieManagerAndroid;
 let CookieManager;
 
 if (Platform.OS === 'ios') {
-    invariant(RNCookieManagerIOS,
-        'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project');
-    CookieManager = RNCookieManagerIOS;
+  invariant(RNCookieManagerIOS,
+            'react-native-cookies: Add RNCookieManagerIOS.h and RNCookieManagerIOS.m to your Xcode project');
+  CookieManager = RNCookieManagerIOS;
 } else if (Platform.OS === 'android') {
-    invariant(RNCookieManagerAndroid,
-        'react-native-cookies: Import libraries to android "react-native link react-native-cookies"');
-    CookieManager = RNCookieManagerAndroid;
+  invariant(RNCookieManagerAndroid,
+            'react-native-cookies: Import libraries to android "react-native link react-native-cookies"');
+  CookieManager = RNCookieManagerAndroid;
 } else {
-    invariant(CookieManager, 'react-native-cookies: Invalid platform. This library only supports Android and iOS.');
+  invariant(CookieManager, 'react-native-cookies: Invalid platform. This library only supports Android and iOS.');
 }
 
 const functions = [
-    'setFromResponse',
-    'getFromResponse',
-    'clearByName'
+  'setFromResponse',
+  'getFromResponse',
+  'clearByName'
 ];
 
 module.exports = {
-  getAll: (useWebKit = false) => CookieManager.getAll(useWebKit),
-  clearAll: (useWebKit = false) => CookieManager.clearAll(useWebKit),
-  get: (url, useWebKit = false) => CookieManager.get(url, useWebKit),
-  set: (cookie, useWebKit = false) => CookieManager.set(cookie, useWebKit),
+  CookieManager: {
+    getAll: (useWebKit = false) => CookieManager.getAll(useWebKit),
+    clearAll: (useWebKit = false) => CookieManager.clearAll(useWebKit),
+    get: (url, useWebKit = false) => CookieManager.get(url, useWebKit),
+    set: (cookie, useWebKit = false) => CookieManager.set(cookie, useWebKit),
+  },
 };
 
 for (var i = 0; i < functions.length; i++) {
-    module.exports[functions[i]] = CookieManager[functions[i]];
+  module.exports.CookieManager[functions[i]] = CookieManager[functions[i]];
 }
